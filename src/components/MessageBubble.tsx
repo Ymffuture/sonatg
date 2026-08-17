90529625281e574b30ff1b308593f0508fac5df0
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-08-17 04:58:56 +0200

    Update MessageBubble.tsx

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -1123,7 +1123,7 @@ export function Bubble({
             {parentBody !== undefined && (
               <div
                 className={`mb-1.5 rounded-lg border-l-[1px] border-[#E07A5F] px-2 py-1.5 text-[11px] ${
-                  mine ? "bg-black/10" : "bg-[red] dark:bg-white/5"
+                  mine ? "bg-black/10" : "bg-[#8C8C8C]/10 dark:bg-white/5"
                 }`}
               >
                 <div className="font-semibold italic text-[#E07A5F] text-[11px]">{parentName}</div>
