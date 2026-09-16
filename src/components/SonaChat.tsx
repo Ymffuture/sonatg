bd24c63e56eaec280780a99bfa6365323794c40f
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-09-16 07:04:21 +0200

    Add files via upload

--- src/components/SonaChat.tsx
+++ src/components/SonaChat.tsx
@@ -4092,7 +4092,6 @@ const [headerMenuView, setHeaderMenuView] = useState<"root" | "more">("root");
       {showAdComposer && activeId && me && (
         <AdComposerModal
           meId={me.id}
-          chatId={activeId}
           onClose={() => setShowAdComposer(false)}
           onCreated={onAdCreated}
         />
